7ce5ac9cf509b1c8af666dc489735a4e4fbc713f
ernestdev <ernestbecedom@gmail.com>
2026-03-11 17:16:37 -0600

    Add: configuration of security for spring security

---
+++ src/main/java/com/nuevaera/api/configuration/Security.java
@@ -0,0 +1,25 @@
+package com.nuevaera.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf(
+                csrf -> csrf.disable()
+        ).authorizeHttpRequests(
+                (auth) ->{
+                    auth.requestMatchers("/api/v1/roles/**").permitAll()
+                            .anyRequest().authenticated();
+                }
+        ).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
